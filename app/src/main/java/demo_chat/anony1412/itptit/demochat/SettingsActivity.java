70ff99603f46f787821e3f73cec002cd81152c78
YOUR NAME <YOUR EMAIL>
2018-07-06 11:10:44 +0700

    // Đang hoàn thiện tính năng cập nhật ảnh đại diện // Hoàn thành update image lên Storage // Tại sao không thể gọi hàm getDownloadUrl() trong StorageReference // Không thể get url image để put vào trường "image" trong DataBase

--- app/src/main/java/demo_chat/anony1412/itptit/demochat/SettingsActivity.java
+++ app/src/main/java/demo_chat/anony1412/itptit/demochat/SettingsActivity.java
@@ -1,16 +1,20 @@
 package demo_chat.anony1412.itptit.demochat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,15 +22,23 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //FireBase
     private DatabaseReference mUserDatabase;
     private FirebaseUser mCurrentUser;
+
+    // Storage FireBase
+    private StorageReference mImageStore;
 
     // Android layout
     private TextView txt_settingDisplayName;
@@ -34,25 +46,36 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     private Button btn_settingChangeImage;
     private Button btn_settingChangeStatus;
     private CircleImageView img_settingDisplayImage;
+
+    //Toolbar
     private Toolbar mToolbar;
 
+    //Progress Dialog
+    private ProgressDialog mProgress;
+
     private static final int GALLERY_PICK = 1;
+    private static final int MAX_LENGTH = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        //Storage FireBase
+        mImageStore = FirebaseStorage.getInstance().getReference();
+
         Setup();
     }
 
     private void Maps() {
+        //Android Layout
         txt_settingDisplayName = findViewById(R.id.txt_settings_displayName);
         txt_settingStatus = findViewById(R.id.txt_settings_status);
         btn_settingChangeImage = findViewById(R.id.btn_settings_changeImage);
         btn_settingChangeStatus = findViewById(R.id.btn_settings_changeStatus);
         img_settingDisplayImage = findViewById(R.id.img_setting_displayImage);
 
+        //Toolbar
         mToolbar = findViewById(R.id.setting_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Setting Account");
@@ -94,16 +117,16 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         int id = view.getId();
         switch (id) {
-            case R.id.btn_settings_changeStatus : {
+            case R.id.btn_settings_changeStatus: {
 
                 String status_value = txt_settingStatus.getText().toString();
                 Intent statusIntent = new Intent(getApplicationContext(), StatusActivity.class);
                 statusIntent.putExtra("status_value", status_value);
                 startActivity(statusIntent);
                 break;
-                
+
             }
-            case R.id.btn_settings_changeImage : {
+            case R.id.btn_settings_changeImage: {
 
                 Intent galleryIntent = new Intent();
                 galleryIntent.setType("image/*");
@@ -126,7 +149,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
-            CropImage.activity(imageUri).setAspectRatio(1,1)
+            CropImage.activity(imageUri).setAspectRatio(1, 1)
                     .start(this);
         }
 
@@ -136,14 +159,68 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
             if (resultCode == RESULT_OK) {
 
+                mProgress = new ProgressDialog(this);
+                mProgress.setTitle("Updating Image");
+                mProgress.setMessage("Please wait while updating image!");
+                mProgress.setCanceledOnTouchOutside(false);
+                mProgress.show();
+
+                String current_uid = mCurrentUser.getUid();
+
                 Uri resultUri = result.getUri();
+                StorageReference filePath = mImageStore.child("profile_images").child(current_uid + ".jpg");
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            // why can't call method getDownloadUrl() inside StorageFireBase???
+                            String download_url = task.getResult().getMetadata().getReference().getDownloadUrl().toString();
+                            mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    mProgress.dismiss();
+                                    Toast.makeText(SettingsActivity.this, "Success Updating", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(SettingsActivity.this, "Upload image error!", Toast.LENGTH_SHORT).show();
+                            mProgress.dismiss();
+                        }
+                    }
+                });
+
+                /*
+                filePath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                });
+                */
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
 
                 Exception error = result.getError();
-                
+
             }
         }
     }
 
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
 }
+
